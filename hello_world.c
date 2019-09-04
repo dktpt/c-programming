9272ea62bdae6759461f01f47c8acabcd38ff689
Dineshkumar <dinesh@savarinetworks.com>
2019-09-04 12:26:29 +0530

    Hello World Program

---
+++ hello_world.c
@@ -0,0 +1,31 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  hello_world.c
+ *
+ *    Description:  prints hello world on the screen
+ *
+ *        Created:  Wednesday 04 September 2019 12:24:55  IST
+ *       Compiler:  gcc
+ *
+ *         Author:  Dineshkumar (), dktpt@outlook.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+
+#include    <stdio.h>
+#include    <stdlib.h>
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  main
+ *  Description:  prints hello world on the screen
+ * =====================================================================================
+ */
+int main ( int argc, char *argv[] )
+{
+    printf("Hello, World\n");
+    return EXIT_SUCCESS;
+}				/* ----------  end of function main  ---------- */
